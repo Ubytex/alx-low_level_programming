9bd99f358ba1fe452c9d7f8e1a8ef90767b2d94b
Ubytex <ubyzone54@gmail.com>
2022-09-11 08:13:40 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,29 +6,42 @@
  */
 int main(void)
 {
-	int i, j;
+	int i, e, g, h, op1, op2;
 
-	for (i = 0; i < 100; i++)
+	i = e = g = h = 48;
+	while (h < 58)
 	{
-		for (j = 0; j < 100; j++)
+		g = 48;
+		while (g < 58)
 		{
-			if (i < J)
+			e = 48;
+			while (e < 58)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
+				i = 48;
+				while (i < 58)
 				{
-					putchar(',');
-					putchar(' ');
+					op1 = (h * 10) + g;
+					op2 = (e * 10) + i;
+					if (op1 < op2)
+					{
+						putchar(h);
+						putchar(g);
+						putchar(' ');
+						putchar(e);
+						putchar(i);
+						if	(h == 57 && g == 56 && e == 57 && i == 57)
+							break;
+						putchar(',');
+						putchar(' ');
+					}
+					i++;
 				}
+				e++;
 			}
+			g++;
 		}
+		h++;
 	}
-
 	putchar('\n');
-
 	return (0);
 }
